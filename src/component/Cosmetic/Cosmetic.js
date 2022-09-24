1449b0ce92cfd70c1907b2fee6a042fbfc845ef2
saklainMostake <saklain15-12003@diu.edu.bd>
2022-09-24 20:14:22 +0600

    cart

--- src/component/Cosmetic/Cosmetic.js
+++ src/component/Cosmetic/Cosmetic.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic
+    const addToCart = (id) =>{
+        console.log('item added', id)
+    } 
+    const addToCartwithParam = () => addToCart(id)
     return (
-        <div>
+        <div className='product'>
             <h1>Buy This: {name} </h1>
             <p>Only For: $ {price} </p>
-
             <p><small>It Has Id: {id}</small></p>
+            <button onClick={addToCartwithParam}>Add to Cart</button> <br />
+            {/* use this */}
+            <button onClick={() => addToCart(id)}>Add to Cart short cut</button>
         </div>
     );
 };
